2d74a76b6b0f936248b44d0fd499bd9991bfc969
Kabilan1506 <mkabilan2878@gmail.com>
2025-04-30 15:06:49 +0530

    index.js file

---
+++ index.js/mathi.js
@@ -0,0 +1,43 @@
+// // let a=8
+// // let b=21
+// // console.log(a+b);
+// // let x=10
+// // let y=5
+// // console.log("addition:"+ (x+y));
+// // console.log("subtraction:"+ (x-y));
+// // console.log("multiplication:"+ (x*y));
+// // console.log("division:"+ (x/y));
+// // let name ="bro!";
+// // console.log("vannakam" + name); 
+// // let love =true;
+// // if (love) {
+// //     console.log("love you macha!")
+// // }
+// // else {
+// //     console.log("next frd da!");
+
+// // }
+// // let num =50;
+// // if (num) {
+// // console.log("it is an odd num");
+// // }
+// // else{
+// // console.log("it is an even num");
+// // }
+// // function addnumbers(a,b) {
+// //     return a+b;
+// // }
+// // let result = addnumbers(5,3);
+// // console.log(result);
+// // const num1 = 4;
+// // const num2 = 15;
+// // const num3 = 2030;
+// // console.log("the first number is "+ num1+"the second number is "+num2+"the third number is "+num3);
+// const numbers = [1,2,3,4,5];
+// const answer = nu(sum,num) => sum + num ,0);
+// const wer numbers.map(num => num *2);
+// const ans = numbers.filter(num => num % 2 === 0);
+// const name = "kabilan";
+// const age =21;
+// const message = `my name is ${name} and my age is ${age}`;
+// console.log (message);
